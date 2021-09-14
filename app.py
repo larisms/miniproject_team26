f21f1ea135c1fb62efe732dfebd18ccbfed79fa4
kancho06 <90511922+kancho06@users.noreply.github.com>
2021-09-14 21:42:10 +0900

    포스트 연결된버전
    굿

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
-import datetime
+from datetime import  datetime
 import hashlib
 import requests
 from bs4 import BeautifulSoup
@@ -10,13 +10,15 @@ from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
-db = client.dbhomework
+db = client.dbhomework_week1
 
 
 ## HTML을 주는 부분
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
 
 
 @app.route('/main', methods=['GET'])
@@ -36,7 +38,11 @@ def listing():
 
 
 ## API 역할을 하는 부분
-@app.route('/main', methods=['POST'])
+@app.route('/create')
+def create():
+    return render_template("create.html")
+
+@app.route('/create', methods=['POST'])
 def saving():
     area_receive = request.form['area_give']
     time_receive = request.form['time_give']
@@ -45,21 +51,34 @@ def saving():
     map_url_receive = request.form['map_url_give']
     file = request.files["file_give"]
 
-    save_to = 'static/mypicture.jpg'
+    extension = file.filename.split('.')[-1]
+
+    today = datetime.now()
+    mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
+    filename = f'file-{mytime}'
+
+    save_to = f'static/{filename}.{extension}'
     file.save(save_to)
 
     doc = {
-        'area_give': area_receive,
-        'time_give': time_receive,
-        'title_give': title_receive,
-        'comment_give': comment_receive,
-        'map_url_give': map_url_receive,
-        'file_give': file
+        'area': area_receive,
+        'time': time_receive,
+        'title': title_receive,
+        'comment': comment_receive,
+        'map_url': map_url_receive,
+        'file': f'{filename}.{extension}'
+
     }
 
     db.walkPlace.insert_one(doc)
 
     return jsonify({'msg': '저장이 완료되었습니다.'})
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
