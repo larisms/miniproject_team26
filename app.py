7d34b6be2163bdccc6a3d906dd90ad2842ee830d
larisms <85733702+larisms@users.noreply.github.com>
2021-09-18 00:34:03 +0900

    hanghae99 week1 miniproject team26
    hanghae99 week1 miniproject team26

--- app.py
+++ app.py
@@ -7,18 +7,12 @@ import jwt
 import hashlib
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime, timedelta
-
-from bson.objectid import ObjectId
-from pymongo import MongoClient
-
-client = MongoClient('localhost', 27017)
-db = client.dbhomework_test
-col = db.likes
+from datetime import  datetime, timedelta
 
 
-
-SECRET_KEY = 'SPARTA'
+SECRET_KEY = config.SECRET_KEY
+client = config.client
+db = config.db
 
 
 ## HTML을 주는 부분
@@ -26,6 +20,8 @@ SECRET_KEY = 'SPARTA'
 # def home():
 #     return render_template('index.html')
 
+
+###### 여기서부터 로그인/회원가입/토큰
 @app.route('/')
 def home():
     token_receive = request.cookies.get('mytoken')
@@ -38,13 +34,9 @@ def home():
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
-
-
-## 로그인
 @app.route('/login')
 def login():
     return render_template('login.html')
-
 
 ## 로그인
 @app.route('/sign_in', methods=['POST'])
@@ -58,8 +50,8 @@ def sign_in():
 
     if result is not None:
         payload = {
-            'id': username_receive,
-            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+         'id': username_receive,
+         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
 
@@ -82,16 +74,15 @@ def sign_up():
     password_receive = request.form['password_give']
     password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
     doc = {
-        "username": username_receive,  # 아이디
-        "password": password_hash,  # 비밀번호
-        "profile_name": username_receive,  # 프로필 이름 기본값은 아이디
-        "profile_pic": "",  # 프로필 사진 파일 이름
-        "profile_pic_real": "profile_pics/profile_placeholder.png",  # 프로필 사진 기본 이미지
-        "profile_info": ""  # 프로필 한 마디
+        "username": username_receive,                               # 아이디
+        "password": password_hash,                                  # 비밀번호
+        "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
+        "profile_pic": "",                                          # 프로필 사진 파일 이름
+        "profile_pic_real": "profile_pics/profile_placeholder.png", # 프로필 사진 기본 이미지
+        "profile_info": ""                                          # 프로필 한 마디
     }
     db.users.insert_one(doc)
     return jsonify({'result': 'success'})
-
 
 @app.route('/sign_up/check_dup', methods=['POST'])
 def check_dup():
@@ -99,6 +90,11 @@ def check_dup():
     exists = bool(db.users.find_one({"username": username_receive}))
     return jsonify({'result': 'success', 'exists': exists})
 
+
+
+
+
+###### 여기서부터 메인페이지
 
 # 메인페이지 카드 리스트
 @app.route('/matjip', methods=['GET'])
@@ -108,12 +104,29 @@ def listing():
     return jsonify({'result': 'success', 'matjip_list': matjip_list})
 
 
-# 디테일 - 09. 17추가
+@app.route('/like_matjip', methods=["POST"])
+def like_matjip():
+    title_receive = request.form["title_give"]
+    address_receive = request.form["address_give"]
+    action_receive = request.form["action_give"]
+    print(title_receive, address_receive, action_receive)
+
+    if action_receive == "like":
+        db.matjips.update_one({"title": title_receive, "address": address_receive}, {"$set": {"liked": True}})
+    else:
+        db.matjips.update_one({"title": title_receive, "address": address_receive}, {"$unset": {"liked": False}})
+    return jsonify({'result': 'success'})
+
+
+
+
+
+
+
+#### 여기서부터 디테일 페이지
 @app.route('/detail')
 def detail():
-
     return render_template('detail.html')
-
 
 # 디테일포스팅 - 09.17추가
 @app.route('/posting', methods=['POST'])
@@ -136,8 +149,6 @@ def posting():
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
 
-
-
 @app.route("/get_posts", methods=['GET'])
 def get_posts():
     token_receive = request.cookies.get('mytoken')
@@ -152,10 +163,6 @@ def get_posts():
         return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다.", "posts" : posts})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
-
-
-
-
 
 
 @app.route('/update_like', methods=['POST'])
@@ -186,62 +193,7 @@ def update_like():
 
 
 
-@app.route('/detail/list', methods=['GET'])
-def comment_listing():
-    comments = list(db.comment.find({}, {'_id': False}))
 
-    return jsonify({'result': 'success', 'comment_list': comments})
-
-
-# 코멘트 포스팅
-@app.route('/detail/list', methods=['post'])
-def save_comment():
-    name_receive = request.form["name_give"]
-    comment_receive = request.form["comment_give"]
-
-    doc = {
-        "name": name_receive,
-        "comment": comment_receive
-
-    }
-
-    db.comment.insert_one(doc)
-
-
-#
-
-
-# @app.route('/create', methods=['POST'])
-# def saving():
-#     area_receive = request.form['area_give']
-#     time_receive = request.form['time_give']
-#     title_receive = request.form['title_give']
-#     comment_receive = request.form['comment_give']
-#     map_url_receive = request.form['map_url_give']
-#     file = request.files["file_give"]
-#
-#     extension = file.filename.split('.')[-1]
-#
-#     today = datetime.now()
-#     mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
-#     filename = f'file-{mytime}'
-#
-#     save_to = f'static/{filename}.{extension}'
-#     file.save(save_to)
-#
-#     doc = {
-#         'area': area_receive,
-#         'time': time_receive,
-#         'title': title_receive,
-#         'comment': comment_receive,
-#         'map_url': map_url_receive,
-#         'file': f'{filename}.{extension}'
-#
-#     }
-#
-#     db.walkPlace.insert_one(doc)
-
-# return jsonify({'msg': '저장이 완료되었습니다.'})
 
 
 if __name__ == '__main__':
